59bff201d25dd6093316f6fd8056dea333ad775e
ElieBamunoba <eliebamunoba@gmail.com>
2023-01-24 22:09:17 +0300

    template

--- flaskblog.py
+++ flaskblog.py
@@ -1,20 +1,30 @@
-from flask import Flask 
+from flask import Flask, render_template
 app=Flask(__name__)
 
+
+posts=[
+    {
+        'author':'Corey Scafer',
+        'title':'Blog Post 1',
+        'Content':'First post content',
+        'date_posted':'April 20,2018',
+    },
+    {
+        'author':'Elie',
+        'title':'Blog Post 2',
+        'Content':'second post content',
+        'date_posted':'April 24,2018',
+    }
+]
 #home page 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h3> Hello World!</h3>'
+    return render_template('home.html', posts=posts)
 
 #the about page 
 @app.route('/about')
 def about():
-    return '<h3> This is about page</h3>'
-
-
-
-
-
+    return render_template('about.html', title="About")
 if __name__=='__main__':
     app.run(debug=True)
